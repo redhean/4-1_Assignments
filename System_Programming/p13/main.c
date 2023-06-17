d21def64a12e12af43c9e3e4b56f5d645fbba4af
redhean <48268986+redhean@users.noreply.github.com>
2023-06-17 14:51:12 +0900

    p13

---
+++ System_Programming/p13/main.c
@@ -0,0 +1,53 @@
+#include <math.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int count;
+int points;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+void *montecarlo(void *arg) {
+    double x, y;
+    /* your code here */
+    unsigned int seed = (unsigned int)(long)arg;
+
+    for(int i = 0; i < points; i++){
+    	x = (double)rand_r(&seed) / RAND_MAX;
+    	y = (double)rand_r(&seed) / RAND_MAX;
+
+	if((x * x) + (y * y) < 1){
+		pthread_mutex_lock(&m);
+		count++;
+		pthread_mutex_unlock(&m);
+	}
+    }
+
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("usage: ./p13 [num_threads] [points per thread]\n");
+        exit(1);
+    }
+    int n = atoi(argv[1]);
+    points = atoi(argv[2]);
+    /* your code here */
+    
+    pthread_t tid[n];
+    long i;
+    for(i = 0; i < n; i++){
+	if(pthread_create(&tid[i], NULL, montecarlo, (void*)i)){
+		exit(1);
+	}
+    }
+
+    for(i = 0; i < n; i++){
+	pthread_join(tid[i], NULL);
+    }
+				
+    double pi = (double)(4 * count) / (double)(n * points);
+    printf("pi: %lf\n", pi);
+    return 0;
+}
